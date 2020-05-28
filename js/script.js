c2897bfe35301de90ef8ba5befd5589af9f7d0eb
Sergey <ilyasovsa93@gmail.com>
2020-05-28 15:06:12 +0300

    some refactoring

--- js/script.js
+++ js/script.js
@@ -1,45 +1,41 @@
-"use strict";
-
-
+// Examples of code
+"use strict"; // Using modern style
 //document.write(1,2,3);
 
 const oldCode = false;
 if (oldCode) {
-    const result2 = confirm('Are you here?');
-    alert('Hello');
+    const confirmResult = confirm('Are you here?'); // message box with OK and Cancel button
+
+    if(confirmResult) {
+        alert('Hello'); // message box with text
+    }
+    else {
+        alert('I see you!');
+    }
+    
 
     let num = 4.555;
-    const answer = +prompt('your age?', '12');
+    const answer = +prompt('your age?', '12'); // message box with ansver, + is return num format
+
     const ff = {
         name: 'John',
         age: 24,
         isMarried: false
     };
-
-
-    console.log(ff);
+    alert(ff);
 
     let ffe = ['ddd', 'ddde', 123];
 
-    let a = 14;
-    console.log(a);
-
-    let number = 5;
     const leftBorderWidth = 1;
-
-    number = 10;
-    console.log(number);
 
     const obj = {
         a: 32
     };
-
-    obj.a = 32;
+    obj.a = 30;
 
     console.log(obj);
 
     console.log(name);
-
     var name = 'Ivan';
     name = 'Alex';
 
