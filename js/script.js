668140b3c5b2fae41bfea3d2e4bd44ac8b862f29
Sergey <ilyasovsa93@gmail.com>
2020-05-28 14:52:18 +0300

    Add first files

---
+++ js/script.js
@@ -0,0 +1,51 @@
+"use strict";
+
+
+//document.write(1,2,3);
+
+const oldCode = false;
+if (oldCode) {
+    const result2 = confirm('Are you here?');
+    alert('Hello');
+
+    let num = 4.555;
+    const answer = +prompt('your age?', '12');
+    const ff = {
+        name: 'John',
+        age: 24,
+        isMarried: false
+    };
+
+
+    console.log(ff);
+
+    let ffe = ['ddd', 'ddde', 123];
+
+    let a = 14;
+    console.log(a);
+
+    let number = 5;
+    const leftBorderWidth = 1;
+
+    number = 10;
+    console.log(number);
+
+    const obj = {
+        a: 32
+    };
+
+    obj.a = 32;
+
+    console.log(obj);
+
+    console.log(name);
+
+    var name = 'Ivan';
+    name = 'Alex';
+
+    {
+        var result = 50;
+    }
+
+    console.log(result);
+}
