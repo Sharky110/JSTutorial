757fdd60b7957d08fdfe4f4eee162bcfe607f38c
Sergey <ilyasovsa93@gmail.com>
2020-05-28 17:16:35 +0300

    create object and write data

--- js/script.js
+++ js/script.js
@@ -2,46 +2,24 @@
 "use strict"; // Using modern style
 //document.write(1,2,3);
 
-const oldCode = false;
-if (oldCode) {
-    const confirmResult = confirm('Are you here?'); // message box with OK and Cancel button
+const numberOfFilms = prompt('Сколько фильмов вы посмотрели?','0');
 
-    if(confirmResult) {
-        alert('Hello'); // message box with text
-    }
-    else {
-        alert('I see you!');
-    }
-    
+const personalMovieDB = {
+    count:numberOfFilms,
+    movies:{},
+    actors:{},
+    genres:[],
+    private: false
+};
 
-    let num = 4.555;
-    const answer = +prompt('your age?', '12'); // message box with ansver, + is return num format
+const lastMovie1 = prompt('Один из последних фильмов?','');
+const movieScope1 = prompt('Его оценка?','');
 
-    const ff = {
-        name: 'John',
-        age: 24,
-        isMarried: false
-    };
-    alert(ff);
+const lastMovie2 = prompt('Один из последних фильмов?','');
+const movieScope2 = prompt('Его оценка?','');
 
-    let ffe = ['ddd', 'ddde', 123];
+personalMovieDB.movies[lastMovie1] = movieScope1;
+personalMovieDB.movies[lastMovie2] = movieScope2;
 
-    const leftBorderWidth = 1;
 
-    const obj = {
-        a: 32
-    };
-    obj.a = 30;
-
-    console.log(obj);
-
-    console.log(name);
-    var name = 'Ivan';
-    name = 'Alex';
-
-    {
-        var result = 50;
-    }
-
-    console.log(result);
-}
+console.log(personalMovieDB);
